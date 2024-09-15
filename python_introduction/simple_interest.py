7bf50bbe16f192415c9409c1494947b33bc3efbe
Wanjiku <wanjikusharon59@gmail.com>
2024-09-15 14:09:02 +0300

    simple intrest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000
+rate = 0.05
+time = 3
+# calculate simple intrest
+interest = (principal * rate * time)
+# print the calculated interest 
+print(f"The simple intrest is: {interest:2f}")
