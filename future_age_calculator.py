a8d01571ce14444d5f0e8360c24c22b3eeeb0617
Wanjiku <wanjikusharon59@gmail.com>
2024-09-15 17:53:10 +0300

    age

--- future_age_calculator.py
+++ future_age_calculator.py
@@ -1,3 +1,3 @@
-current_age = int (input ("How old are you?"))
+current_age = int (input ("how old are you?"))
 age_in_2050 = current_age + 27
 print ("In 2050, you will be", age_in_2050, "years old")
