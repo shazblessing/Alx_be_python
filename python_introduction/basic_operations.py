3f422dce789c7a58d447dda0e88044ff340b2429
Wanjiku <wanjikusharon59@gmail.com>
2024-09-15 12:04:17 +0300

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+number1 = 10
+number2 = 5
+# performing basic arithmetic operations
+sum = number1 + number2
+subtraction = number1 - number2
+product = number1 * number2
+print(f"sum;{sum},subtraction;{subtraction},product;{product}")
