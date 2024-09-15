f144e4ecfe3874e620d065b2ca7b684c51757f2a
Wanjiku <wanjikusharon59@gmail.com>
2024-09-15 14:45:48 +0300

    amended age in 2050

--- future_age_calculator.py
+++ future_age_calculator.py
@@ -5,4 +5,4 @@ current_age = int (input("How old are you?") )
 # calculate age in current year 
 age_in_2050 = current_age + (target_year - current_year)
 # print the result with the formatted string
-print(f"{target_year}, you will be {age_in_2050} years old.")
+print(f"In {target_year}, you will be {age_in_2050} years old.")
